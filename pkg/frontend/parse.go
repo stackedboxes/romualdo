f0acc6b426130a89fa0454207a5aa1b1274e131f
Leandro Motta Barros <lmb@stackedboxes.org>
2023-03-17 21:11:44 -0300

    Implement the public frontend.Parse() function

--- pkg/frontend/parse.go
+++ pkg/frontend/parse.go
@@ -7,12 +7,19 @@
 
 package frontend
 
-import "github.com/stackedboxes/romualdo/pkg/ast"
+import (
+	"github.com/stackedboxes/romualdo/pkg/ast"
+)
 
 // Parse parses Romualdo Language source code and returns its AST (Abstract
 // Syntax Tree). In case of errors, returns nil and prints the error messages to
 // the standard error.
 func Parse(source string) ast.Node {
-	// TODO: Implement!
-	return nil
+	p := newParser(source)
+	root := p.parse()
+	if root == nil {
+		return nil
+	}
+
+	return root
 }
