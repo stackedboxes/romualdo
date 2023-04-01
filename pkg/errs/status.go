af3425689f747f080c638ff4bb6b2ca2b301ac47
Leandro Motta Barros <lmb@stackedboxes.org>
2023-03-31 22:21:58 -0300

    Be more consistent with return status codes

--- pkg/errs/status.go
+++ pkg/errs/status.go
@@ -5,18 +5,19 @@
 * Licensed under the MIT license (see LICENSE.txt for details)                 *
 \******************************************************************************/
 
-package main
+package errs
 
-import (
-	"fmt"
-	"os"
+const (
+	// StatusCodeSuccess indicates a successful execution.
+	StatusCodeSuccess = 0
 
-	"github.com/stackedboxes/romualdo/pkg/errs"
+	// StatusCodeCompileTimeError indicates a compile-time error.
+	StatusCodeCompileTimeError = 1
+
+	// StatusCodeTestSuiteError indicates a failure while running Romualdo's own
+	// test suite.
+	StatusCodeTestSuiteError = 2
+
+	// StatusCodeICE indicates an Internal Compiler Error.
+	StatusCodeICE = 125
 )
-
-func main() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Internal Compiler Error: unexpected top-level error:%v\n", err)
-		os.Exit(errs.StatusCodeICE)
-	}
-}
--- pkg/errs/util.go
+++ pkg/errs/util.go
@@ -22,21 +22,26 @@ func ReportAndExit(err error) {
 	iceErr := &ICE{}
 	switch {
 	case err == nil:
-		os.Exit(0)
+		os.Exit(StatusCodeSuccess)
+
 	case errors.As(err, &compTimeError):
 		fmt.Printf("%v\n", compTimeError)
-		os.Exit(1) // TODO: Document what each status code means.
-	case errors.As(err, &testSuiteError):
-		fmt.Printf("%v\n", testSuiteError)
-		os.Exit(2) // TODO: Document what each status code means.
+		os.Exit(StatusCodeCompileTimeError)
+
 	case errors.As(err, &compTimeColl):
 		fmt.Printf("%v", compTimeColl)
-		os.Exit(1) // TODO: Document what each status code means.
+		os.Exit(StatusCodeCompileTimeError)
+
+	case errors.As(err, &testSuiteError):
+		fmt.Printf("%v\n", testSuiteError)
+		os.Exit(StatusCodeTestSuiteError)
+
 	case errors.As(err, &iceErr):
 		fmt.Printf("Internal Compiler Error: %v\n", iceErr)
-		os.Exit(171) // TODO: Document what each status code means.
+		os.Exit(StatusCodeICE)
+
 	default:
 		fmt.Printf("Internal Compiler Error: unexpected error of type %T: %v\n", err, err)
-		os.Exit(171) // TODO: Document what each status code means.
+		os.Exit(StatusCodeICE)
 	}
 }
