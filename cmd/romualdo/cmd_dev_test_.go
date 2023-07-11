1beaf982b4f4892a6b958c978eda51e589b7d953
Leandro Motta Barros <lmb@stackedboxes.org>
2023-07-11 18:37:37 -0300

    Start redesigning the testing system
    Added a doc explaining how I intend it to be, and started to move the
    implementation in that direction.

--- cmd/romualdo/cmd_dev_test_.go
+++ cmd/romualdo/cmd_dev_test_.go
@@ -22,8 +22,24 @@ import (
 	"github.com/stackedboxes/romualdo/pkg/vm"
 )
 
+type testStep struct {
+	Type          string
+	SourceDir     string
+	Input         []string
+	Output        []string
+	ExitCode      int
+	ErrorMessages []string
+}
+
 type testConfig struct {
-	ExpectedOutput []string
+	Type          string
+	SourceDir     string
+	Input         []string
+	Output        []string
+	ExitCode      int
+	ErrorMessages []string
+
+	Steps []testStep `toml:"step"`
 }
 
 // swRunnerFunc is a function that can run a Storyworld at path, using mouth and
@@ -52,31 +68,7 @@ var devTestCmd = &cobra.Command{
 
 		err := romutil.ForEachMatchingFileRecursive(flagDevTestSuite, regexp.MustCompile("test.toml"),
 			func(configPath string) errs.Error {
-				testConf, err := readTestConfig(configPath)
-				if err != nil {
-					rtErr := errs.NewRomualdoTool("reading test config file: %v", err)
-					return rtErr
-				}
-
-				testPath := path.Dir(configPath)
-				srcPath := path.Join(testPath, "src")
-				outBuilder := &strings.Builder{}
-				mouth := romutil.NewWriterMouth(outBuilder)
-				ear := romutil.NewReaderEar(os.Stdin) // TODO: Must come from test config!
-
-				err = runner(srcPath, mouth, ear)
-				if err != nil {
-					return errs.NewTestSuite(testPath, "running the storyworld: %v", err)
-				}
-
-				actualOut := outBuilder.String()
-				if actualOut != testConf.ExpectedOutput[0] {
-					errTS := errs.NewTestSuite(testPath, "expected output '%v', got '%v'.", testConf.ExpectedOutput[0], actualOut)
-					return errTS
-				}
-
-				fmt.Printf("Test case passed: %v.\n", testPath)
-				return nil
+				return runTestCase(configPath, runner)
 			},
 		)
 		reportAndExit(err)
@@ -98,8 +90,43 @@ func init() {
 		false, "Test using the walk tree interpreter instead of the bytecode one")
 }
 
+// runTestCase runs the test case defined in configPath using the given runner.
+func runTestCase(configPath string, runner swRunnerFunc) errs.Error {
+	testPath := path.Dir(configPath)
+	testCase := testPath
+
+	testConf, err := readTestConfig(configPath)
+	if err != nil {
+		return err
+	}
+	canonicalizeTestConfig(testConf)
+	err = validateConfig(testCase, testConf)
+	if err != nil {
+		return err
+	}
+
+	srcPath := path.Join(testPath, "src")
+	outBuilder := &strings.Builder{}
+	mouth := romutil.NewWriterMouth(outBuilder)
+	ear := romutil.NewReaderEar(os.Stdin) // TODO: Must come from test config!
+
+	err = runner(srcPath, mouth, ear)
+	if err != nil {
+		return errs.NewTestSuite(testCase, "running the storyworld: %v", err)
+	}
+
+	actualOut := outBuilder.String()
+	if actualOut != testConf.Output[0] {
+		errTS := errs.NewTestSuite(testCase, "expected output '%v', got '%v'.", testConf.Output[0], actualOut)
+		return errTS
+	}
+
+	fmt.Printf("Test case passed: %v.\n", testPath)
+	return nil
+}
+
 // readTestConfig reads a test configuration from a TOML file.
-func readTestConfig(path string) (*testConfig, error) {
+func readTestConfig(path string) (*testConfig, errs.Error) {
 	tomlSource, err := os.ReadFile(path)
 	if err != nil {
 		tsErr := errs.NewTestSuite(path, "%v", err.Error())
@@ -113,4 +140,78 @@ func readTestConfig(path string) (*testConfig, error) {
 	}
 
 	return tomlConfigData, nil
+}
+
+// canonicalizeTestConfig makes sure testConf is in the canonical form.
+// Specifically, it:
+//
+//   - Makes sure there is at least one element in Steps. (If there is no
+//     explicit step defined, we create one with the data from the top-level
+//     fields.)
+//   - Makes sure all fields in all Steps have values: either the values
+//     explicitly set or, or the values from the top-level fields, or the
+//     default values.
+func canonicalizeTestConfig(testConf *testConfig) {
+	// Give default values to all empty fields in the top-level config.
+	if testConf.Type == "" {
+		testConf.Type = "build-and-run"
+	}
+	if testConf.SourceDir == "" {
+		testConf.SourceDir = "src"
+	}
+	if testConf.Input == nil {
+		testConf.Input = []string{}
+	}
+	if testConf.Output == nil {
+		testConf.Output = []string{}
+	}
+	if testConf.ErrorMessages == nil {
+		testConf.ErrorMessages = []string{}
+	}
+
+	// Make sure we have one step.
+	if len(testConf.Steps) == 0 {
+		testConf.Steps = append(testConf.Steps, testStep{
+			Type:          testConf.Type,
+			SourceDir:     testConf.SourceDir,
+			Input:         testConf.Input,
+			Output:        testConf.Output,
+			ExitCode:      testConf.ExitCode,
+			ErrorMessages: testConf.ErrorMessages,
+		})
+	}
+
+	// Give values to all fields of all steps.
+	for _, step := range testConf.Steps {
+		if step.Type == "" {
+			step.Type = testConf.Type
+		}
+		if step.SourceDir == "" {
+			step.SourceDir = testConf.SourceDir
+		}
+		if step.Input == nil {
+			step.Input = testConf.Input
+		}
+		if step.Output == nil {
+			step.Output = testConf.Output
+		}
+		if step.ErrorMessages == nil {
+			step.ErrorMessages = testConf.ErrorMessages
+		}
+		if step.ExitCode == 0 && testConf.ExitCode != 0 {
+			step.ExitCode = testConf.ExitCode
+		}
+	}
+}
+
+// validateConfig validates a test configuration that is already in canonical
+// format. Returns nil if the configuration is valid, or an error otherwise.
+func validateConfig(testCase string, testConf *testConfig) errs.Error {
+	for _, step := range testConf.Steps {
+		// Validate step type
+		if step.Type != "build-and-run" {
+			return errs.NewTestSuite(testCase, "invalid test type '%v'; only 'build-and-run' supported for now", step.Type)
+		}
+	}
+	return nil
 }
---
+++ doc/testing.md
@@ -0,0 +1,170 @@
+# Testing
+
+Notes on the format expected by `romualdo dev test`.
+
+**Warning.** Right now, these are more musings on what to make than a
+description of the actual implementation (which is extremely bare and not very
+much like what is described here).
+
+## Concepts
+
+A **test case** is declaratively defined in a `test.toml` file. Each test case
+is comprised of a sequence of **steps** (though many test cases will have one
+single step, and the syntax is simplified for this case).
+
+The term **test suite** is loosely used to describe a set of test cases. It is
+just a directory where we recursively look for `test.toml` files.
+
+### Single-step versus multi-step test cases
+
+Let's start with the simpler case, which is a single-step test case. It would be
+defined like this:
+
+```toml
+# test.toml for a single-step test case
+
+key1 = "value"
+key2 = 123
+key3 = [
+    "one",
+    "two",
+    "three",
+]
+```
+
+The keys and values here are mde up, but the structure is real: a single-step
+test case is just a bunch of key/values at the top-level of the `test.toml`
+file.
+
+A multi-step test case is not very much unlike a single-step one. In TOML terms,
+it contains an array of tables called `step`, in which each member is exactly
+like in the single-step case. Like this:
+
+```toml
+# test.toml for a multi-step test case
+
+# Here's the first step
+[[step]]
+    key1 = "value"
+    key2 = 123
+    key3 = [
+        "one",
+        "two",
+        "three",
+    ]
+
+# And here's the second one
+[[step]]
+    key1 = "another value"
+    key2 = 123
+    key3 = [
+        "1",
+        "22",
+        "333",
+    ]
+```
+
+There's another handy possibility with multi-step test cases: any keys you
+define at top-level are used as defaults for the actual steps. The top-level
+keys do not define a step, they are just a handy way to share values between
+steps.
+
+```toml
+# test.toml for a multi-step test case with shared defaults
+
+# These are just default values, not a real step.
+key2 = 123
+key3 = [
+    "A",
+    "B",
+    "C",
+]
+
+# This is the first step. It "inherits" the key2 and key3 values from the
+# top-level declarations above.
+[[step]]
+    key1 = "value"
+
+# And this is the second step. It "inherits" key3 from the top-level
+# declarations, but overrides key2.
+[[step]]
+    key1 = "another value"
+    key2 = 321
+```
+
+## Reference
+
+These are the keys recognized by `romualdo dev test`.
+
+### `type`
+
+*Default: `build-and-run`.*
+
+Steps can be of different types, depending on what you want the step to do. So,
+in a sense this is the most important key, as it determines even which other
+keys are relevant.
+
+Possible values are:
+
+* `build`: The step builds the source code. It can check for error codes and
+  error messages.
+* `run`: The step runs the Storyworld built on a previous step. It can send
+  input, and check if it gets the expected output.
+* `build-and-run`: Equivalent to `build` followed by `run`; the `run` part is
+  skipped if the build fails.
+* `save-state`: The step saves the VM state.
+* `load-state`: The step loads the VM state (assumed to have been previously
+  saved).
+
+### `sourceDir`
+
+*Valid for:* `build`, `build-and-run`.  
+*Default:* `src`.
+
+Defines the directory where the Storyworld source code will be looked for. This
+is relative to the directory where `test.toml` is.
+
+### `input`
+
+*Valid for:* `run`, `build-and-run`.  
+*Default:* `[]`
+
+An array of strings, which will be send as input to the Storyworld. Each element
+in the array will be sent at a time, for each time the Storyworld `listen`s.
+
+It is expected that all elements in the array will be used when the execution
+completes.
+
+### `output`
+
+*Valid for:* `run`, `build-and-run`.  
+*Default:* `[]`
+
+An array of strings, which represent the expected output from the Storyworld.
+
+`input` and `output` are used in lockstep: first an output is used, then an
+input is send, then a new output is taken, and so on. So, there must be one
+output more than inputs.
+
+### `exitCode`
+
+*Valid for:* All `type`s.  
+*Default:* `0`
+
+This is the expected exit code of the `romualdo` tool when running the test
+case.
+
+It is relevant only for the last step, as all previous steps are expected to be
+successful.
+
+### `errorMessages`
+
+*Valid for:* All `type`s.  
+*Default:* `[]`
+
+This is an array of strings, each of which representing an error message
+expected to be present in the output. Each string is really interpreted as a
+regular expression that must match the standard error.
+
+It is relevant only for the last step, as all previous steps are expected to be
+successful.
